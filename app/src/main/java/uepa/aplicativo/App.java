121d109895a11e605dee5ac3434b720d6e8750fa
enzofarinamullis <e243589@dac.unicamp.br>
2026-07-10 23:49:44 -0300

    feat: Corrigida localizacao de resources
    A pasta resources deve ficar na pasta app/src/main.
    E adiciona um protótipo inicial da tela de login.

--- app/src/main/java/uepa/aplicativo/App.java
+++ app/src/main/java/uepa/aplicativo/App.java
@@ -3,6 +3,7 @@ package uepa.aplicativo;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -15,20 +16,7 @@ public class App extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+        System.out.println(getClass().getResource("/fxml/TelaLogin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(); 
     }
 }
