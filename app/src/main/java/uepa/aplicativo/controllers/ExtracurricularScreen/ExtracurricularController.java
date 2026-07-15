37acb7e8531cb29231c6cd25e6703c85d973a282
enzofarinamullis <e243589@dac.unicamp.br>
2026-07-15 01:42:46 -0300

    feat: adiciona controller e melhora extrascreen
    Adiciona controller e adiciona mais visuais ao extracurricular screen,
    como hyperlink. Funções do controller não implementadas.

---
+++ app/src/main/java/uepa/aplicativo/controllers/ExtracurricularScreen/ExtracurricularController.java
@@ -0,0 +1,32 @@
+package uepa.aplicativo.controllers.ExtracurricularScreen;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.VBox;
+
+public class ExtracurricularController {
+
+    @FXML
+    private Button goBackButton;
+
+    @FXML
+    private Hyperlink hyperlink;
+
+    @FXML
+    private VBox vboxStaff;
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openLink(ActionEvent event) {
+
+    }
+
+}
+
