43a0119256223569ec3397cb8497077295cbe6a7
Victor Marques <a288660@dac.unicamp.br>
2026-07-16 23:57:57 -0300

    Adicionando as Tags

--- app/src/main/java/uepa/aplicativo/message/Message.java
+++ app/src/main/java/uepa/aplicativo/message/Message.java
@@ -1,42 +1,59 @@
 package uepa.aplicativo.message;
+import java.time.LocalDateTime;
 
+import uepa.aplicativo.extracurricular.Extracurricular;
+import uepa.aplicativo.user.Staff;
 public class Message {
-    private String title;
-    private String text;
-    private String creatorName;
+  private String title;
+  private String text;
+  private LocalDateTime date;
+  private Extracurricular extra;
+  private Staff staff;
 
-    public Message(String title, String text, String creatorName) {
-        this.title = title;
-        this.text = text;
-        this.creatorName = creatorName;
+  public Message(String title, String text, Extracurricular extra, Staff staff ) {
+    this.title = title;
+    this.text = text;
+	
+	
+
+    this.date = LocalDateTime.now();
+    this.extra = extra;
+    this.staff = staff;
+  }
+  public String getTitle() {
+        return title;
     }
 
-    public String getTitle() {
-        return title;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getText() {
         return text;
     }
 
-    public String getCreatorName() {
-        return creatorName;
-    }
-
-    public void setTitle(String title) throws Exception{
-        if(title.length() > 20){
-            throw new Exception("Title can not exceed 20 characters");
-        }
-        this.title = title;
-    }
-
-    public void setText(String text) throws Exception{
-        if(text.length() > 200) {
-            throw new Exception("Message text can not exceed 200 characters");
-        }
+    public void setText(String text) {
         this.text = text;
     }
 
-    
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime dataEnvio) {
+        this.date = dataEnvio;
+    }
+    public Extracurricular getExtra() {
+        return extra;
+    }
+    public void setExtra(Extracurricular extra) {
+        this.extra = extra;
+    }
+    public Staff getStaff(){
+        return staff;
+    }
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
 
 }
