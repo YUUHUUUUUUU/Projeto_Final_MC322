3b1d476d89591c6fc2c3007e89d32bb8891ed5a3
enzofarinamullis <e243589@dac.unicamp.br>
2026-07-17 00:36:00 -0300

    feat: interface de recebimento de dados
    Cria uma interface para gerar o contrato para que os controllers tenham
    um método responsável para receber dados.

---
+++ app/src/main/java/uepa/aplicativo/interfaces/RecieveData.java
@@ -0,0 +1,8 @@
+package uepa.aplicativo.interfaces;
+
+import uepa.aplicativo.DataManager.Data;
+
+public interface RecieveData {
+    public void receiveData(Data data);
+    public void setData(Data data);
+}
