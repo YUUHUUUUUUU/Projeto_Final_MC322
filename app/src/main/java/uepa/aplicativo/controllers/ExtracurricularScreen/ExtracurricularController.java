818504b475689699a6f552621bf4e2c17961ff33
enzofarinamullis <e243589@dac.unicamp.br>
2026-07-17 00:37:34 -0300

    chore: remove interface não utilizada
    Remove interface que acabamos decidindo não implementar.

--- app/src/main/java/uepa/aplicativo/controllers/ExtracurricularScreen/ExtracurricularController.java
+++ app/src/main/java/uepa/aplicativo/controllers/ExtracurricularScreen/ExtracurricularController.java
@@ -20,7 +20,7 @@ import uepa.aplicativo.loaders.loadedData.LoadedCard;
 import uepa.aplicativo.message.Message;
 import uepa.aplicativo.user.Staff;
 
-public class ExtracurricularController implements Initializable,Editable {
+public class ExtracurricularController implements Initializable {
 
     @FXML
     private Button goBackButton;
@@ -117,41 +117,6 @@ public class ExtracurricularController implements Initializable,Editable {
 
         return catalogItems;
 
-    }
-
-    @Override
-    public void editExtraName(String newName){
-        extracurricular.setName(newName);
-    }
-
-    @Override
-    public void editExtraDesc(String newDesc){
-        extracurricular.setDescription(newDesc);
-    }
-
-    @Override
-    public void editExtraOpen(boolean newOpen){
-        extracurricular.setOpenToWork(newOpen);
-    }
-
-    @Override
-    public void editExtraLink(String newHyper){
-        extracurricular.setHyperLink(newHyper);
-    }
-
-    @Override
-    public void editExtraInstitute(String newInst){
-        extracurricular.setInstitute(newInst);
-    }
-
-    @Override
-    public void editExtraBanner(String newPath){
-        extracurricular.setBannerPath(newPath);
-    }
-
-    @Override
-    public void editExtraLogo(String newPath){
-        extracurricular.setLogoPath(newPath);
     }
 }
 
