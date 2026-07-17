cdc73eadec3f6ae6c9df0a3cc287cfe36ba7dd9b
enzofarinamullis <e243589@dac.unicamp.br>
2026-07-17 08:18:22 -0300

    style: caco pronto!

--- app/src/main/java/uepa/aplicativo/controllers/ExtracurricularScreen/ExtracurricularController.java
+++ app/src/main/java/uepa/aplicativo/controllers/ExtracurricularScreen/ExtracurricularController.java
@@ -19,6 +19,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -34,6 +35,9 @@ import uepa.aplicativo.message.Message;
 import uepa.aplicativo.user.Staff;
 
 public class ExtracurricularController implements RecieveData{
+
+    @FXML
+    private ImageView banner;
 
     @FXML
     private Label description;
@@ -124,6 +128,7 @@ public class ExtracurricularController implements RecieveData{
         this.name.setText(this.extracurricular.getName());
         this.hyperlink.setText(this.extracurricular.getHyperLink());
         this.type.setText(this.extracurricular.getInstitute());
+        this.banner.setImage(this.extracurricular.getBanner());
         try {
             List<HBox> catalogItems = loadCatalog();
             
